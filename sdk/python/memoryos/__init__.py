0ce3e653cddb29c89c200a06bb905d8db7f59361
Adity <aditykushwaha69@gmail.com>
2026-06-28 18:20:23 +0530

    sdk: export MemorySource and EvidenceReference from package root

--- sdk/python/memoryos/__init__.py
+++ sdk/python/memoryos/__init__.py
@@ -1,5 +1,13 @@
 from memoryos.async_client import AsyncMemory
 from memoryos.client import Memory
+from memoryos.types import EvidenceReference
+from memoryos.types import MemorySource
 from memoryos.universal import UniversalMemory
 
-__all__ = ["AsyncMemory", "Memory", "UniversalMemory"]
+__all__ = [
+    "AsyncMemory",
+    "EvidenceReference",
+    "Memory",
+    "MemorySource",
+    "UniversalMemory",
+]
